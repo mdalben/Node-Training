538aa3f8164d9da39065ba518f9bdf298229793b
micheledalben <michele.dalben@ennova-research.com>
2018-02-21 11:11:07 +0100

    crud express api without db ts added

--- nodejs_backend_typescript/app2/build/index.js
+++ nodejs_backend_typescript/app2/build/index.js
@@ -3,11 +3,64 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 var PORT = 5000;
+app.use(express.json());
 app.set('port', PORT);
 app.get('/', function (request, response) {
     response.send('Hello World!')
         .status(200)
         .end();
+});
+app.get('/user', function (request, response) {
+    var user = {
+        'name': 'John',
+        'surname': 'Smith',
+        'age': 43,
+        'job': 'Policeman'
+    };
+    response.send(user)
+        .status(200)
+        .end();
+});
+app.put('/user', function (request, response, next) {
+    if (!request.body) {
+        next('No user to add');
+    }
+    else {
+        var _a = request.body, name_1 = _a.name, surname = _a.surname, age = _a.age, job = _a.job;
+        response.send({
+            user: {
+                name: name_1,
+                surname: surname,
+                age: age,
+                job: job
+            }
+        }).status(200).end();
+    }
+});
+app.post('/user', function (request, response, next) {
+    if (!request.body)
+        next('No user to update');
+    else {
+        var user = request.body;
+        user.name = 'John';
+        user.surname = 'Smith';
+        user.age = 43;
+        user.job = 'Policeman';
+        response.send(user)
+            .status(200)
+            .end();
+    }
+});
+app.delete('/user', function (request, response, next) {
+    if (!request.body)
+        next('No user to delete');
+    else {
+        var user = request.body;
+        user = {};
+        response.send(user)
+            .status(200)
+            .end();
+    }
 });
 app.listen(app.get('port'), function () {
     console.log("Server is running at port " + PORT);
--- nodejs_backend_typescript/app2/src/index.ts
+++ nodejs_backend_typescript/app2/src/index.ts
@@ -3,12 +3,69 @@ import * as express from 'express'
 let app = express()
 let PORT = 5000
 
+app.use(express.json())
+
 app.set('port', PORT)
 
 app.get('/', function(request : express.Request, response : express.Response) {
-    response.send('Hello World!')
+    response.send('Hello World from express ts!')
             .status(200)
             .end()
+})
+
+app.get('/user', function(request : express.Request, response : express.Response) {
+    let user = {
+        'name' : 'John',
+        'surname' : 'Smith',
+        'age' : 43,
+        'job' : 'Policeman'
+    }
+    response.send(user)
+            .status(200)
+            .end()
+})
+
+app.put('/user', function(request : express.Request, response : express.Response, next : express.Next) {
+    if (!request.body) {
+        next('No user to add')
+    } else {
+        let { name, surname, age, job} = request.body
+        response.send({
+            user : {
+                name,
+                surname,
+                age,
+                job
+            }
+        }).status(200).end()
+    }
+})
+
+app.post('/user', function(request : express.Request, response : express.Response, next : express.Next) {
+    if (!request.body)
+        next('No user to update')
+    else {
+        let user = request.body
+        user.name = 'John'
+        user.surname = 'Smith'
+        user.age = 43
+        user.job = 'Policeman'
+        response.send(user)
+                .status(200)
+                .end()
+    }
+})
+
+app.delete('/user', function(request : express.Request, response : express.Response, next : express.Next) {
+    if (!request.body)
+        next('No user to delete')
+    else {
+        let user = request.body
+        user = {}
+        response.send(user)
+                .status(200)
+                .end()
+    }
 })
 
 app.listen(app.get('port'), function(){
