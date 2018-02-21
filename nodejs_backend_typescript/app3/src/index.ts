4e8d6e67bb6c66c23a64d4f467e55c56dbc642e4
micheledalben <michele.dalben@ennova-research.com>
2018-02-21 14:55:45 +0100

    const app3

--- nodejs_backend_typescript/app3/src/index.ts
+++ nodejs_backend_typescript/app3/src/index.ts
@@ -1,8 +1,8 @@
 import * as express from 'express'
 import * as path from 'path'
 
-let app = express()
-let PORT = 5000
+const app = express()
+const PORT = 5000
 
 app.use(express.static(path.join(__dirname, '../public')))
 
