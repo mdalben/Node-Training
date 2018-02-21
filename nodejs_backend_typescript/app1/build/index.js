37cf0f500ab540885997de419c3169bb102dc0cc
micheledalben <michele.dalben@ennova-research.com>
2018-02-21 10:14:37 +0100

    simple http hello world api with typescript added

--- nodejs_backend_typescript/app1/build/index.js
+++ nodejs_backend_typescript/app1/build/index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http = require("http");
+var app = new http.Server();
+var PORT = 5000;
+app.on('request', function (request, response) {
+    response.writeHead(200, { 'Content-type': 'text-plain' });
+    response.write('Hello World!');
+    response.end();
+});
+app.listen(PORT, function () {
+    console.log("Server is running at port " + PORT);
+});
--- nodejs_backend_typescript/app1/src/index.ts
+++ nodejs_backend_typescript/app1/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+import * as http from "http"
+
+let app = new http.Server()
+let PORT = 5000
+
+app.on('request', function(request : http.ServerRequest, response : http.ServerResponse) {
+    response.writeHead(200, {'Content-type' : 'text-plain'})
+    response.write('Hello World!')
+    response.end()
+})
+
+app.listen(PORT, function() {
+    console.log(`Server is running at port ${PORT}`)
+})
