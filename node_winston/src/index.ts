0551032366e08a43cea18121e308d896d64bf568
Michele Dal Ben <855412@stud.unive.it>
2018-02-23 17:50:07 +0100

    config level log color

--- node_winston/src/index.ts
+++ node_winston/src/index.ts
@@ -2,12 +2,40 @@
 
 const winston = require('winston')
 
+const config = {
+    levels: {
+        error: 0,
+        debug: 1,
+        warn: 2,
+        data: 3,
+        info: 4,
+        verbose: 5,
+        silly: 6
+    },
+    colors: {
+        error: 'red',
+        debug: 'blue',
+        warn: 'yellow',
+        data: 'grey',
+        info: 'green',
+        verbose: 'cyan',
+        silly: 'magenta'
+    }
+}
+/** 
+ * creates a logger via console with color.
+*/
 const logger = winston.createLogger({
+    levels: config.levels,
     transports: [new winston.transports.Console()],
     format: winston.format.combine(
-        winston.format.colorize({all: true}),
+        winston.format.colorize(),
         winston.format.simple()
     )
 })
 
-logger.log('info', 'This is an information message..')
+logger.debug('This is a debug message..')
+logger.warn('This is a warn message..')
+logger.error('This is an error message..')
+logger.data('This is a data message..')
+logger.info('This is an info message..')
