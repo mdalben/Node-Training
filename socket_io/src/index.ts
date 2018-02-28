378873234e217771a45c37e1b7a679da3bf8f4ef
micheledalben <michele.dalben@ennova-research.com>
2018-02-28 15:31:34 +0100

    added socket.io room example

--- socket_io/src/index.ts
+++ socket_io/src/index.ts
@@ -11,19 +11,42 @@ const PORT = 5000
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     response.sendFile('index.html')
 })
 
-io.on('connection', function(socket){
+/**
+ * First example
+ */
+/*io.on('connection', function(socket){
     console.log('Client connected!')
 
     socket.on('join', function(data) {
         console.log(data)
         socket.emit('messages', 'Hello from server!')
     })
+})*/
+
+/**
+ * Second example with socket.io rooms
+ */
+
+let n = 1
+let room_number = 0;
+
+io.on('connection', function (socket) {
+    console.log(`Client #${n} with id ${socket.id} connected!`)
+    n++
+
+    if (io.nsps['/'].adapter.rooms['room-' + room_number] && io.nsps['/'].adapter.rooms['room-' + room_number].length > 0)
+        room_number++
+
+    socket.join('room-' + room_number)
+
+    io.in('room-' + room_number).emit('connectToRoom', 'Welcome to Root n.' + room_number)
+
 })
 
-server.listen(PORT, function(){
+server.listen(PORT, function () {
     console.log(`Server il listening at ${PORT}`)
 })
